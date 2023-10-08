fbadd556e64969840ea98c450704c95e3821fc37
Adriel <adriel.fsantos@outlook.com>
2023-10-08 16:24:38 -0300

    Adiciona fonte para textos comuns

--- fonts.lua
+++ fonts.lua
@@ -4,10 +4,19 @@ local function display(size)
   return love.graphics.newFont("assets/fonts/MajorMonoDisplay-Regular.ttf", size or 20)
 end
 
+local function commonText(size)
+  return love.graphics.newFont("assets/fonts/Urbanist-VariableFont_wght.ttf", size or 20)
+end
+
 return {
   display = {
     small = display(20),
     normal = display(40),
     large = display(60),
+  },
+  commonText = {
+    small = commonText(14),
+    normal = commonText(20),
+    large = commonText(28),
   }
 }
