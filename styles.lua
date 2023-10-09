9e02647c43e959aa2904e28cf2496e1bbabc076a
Adriel <adriel.fsantos@outlook.com>
2023-10-09 00:14:48 -0300

    Adiciona arquivo de estilos

---
+++ styles.lua
@@ -0,0 +1,26 @@
+local fonts = require("fonts")
+
+return {
+  menu = {
+    text = { 1, 1, 1 },
+    button = {
+      font = fonts.commonText.normal,
+      padding = { 16, 12 },
+      radius = { 4, 4 },
+      states = {
+        normal = {
+          bg = { 206 / 255, 218 / 255, 235 / 255 },
+          text_color = { 1, 1, 1 },
+          mode = "line",
+          radius = { 4, 4 },
+        },
+        hover = {
+          bg = { 1, 1, 1 },
+          text_color = { 0, 0, 0 },
+          mode = "fill",
+          padding = { 16, 12 },
+        },
+      }
+    }
+  }
+}
