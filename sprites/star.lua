c0373064354dd54df5ea414deaff18108059b477
Adriel <adriel.fsantos@outlook.com>
2023-11-05 21:54:33 -0300

    Adiciona sprite de estrela

---
+++ sprites/star.lua
@@ -0,0 +1,17 @@
+local love = require("love")
+local anim8 = require('lib.anim8')
+
+local function Star(frameDuration)
+  local d = frameDuration or 0.1
+  local sprite = love.graphics.newImage('assets/sprites/star.png')
+  local grid = anim8.newGrid(64, 64, sprite:getWidth(), sprite:getHeight())
+
+  return {
+    animations = {
+      default = anim8.newAnimation(grid('1-10', 1), d)
+    },
+    sprite = sprite,
+  }
+end
+
+return Star
