be3d06abc304e5e492adc8586a5552a8ccf8fec5
Adriel <adriel.fsantos@outlook.com>
2023-11-23 23:15:33 -0300

    Adiciona mapa inicial

---
+++ maps/world01/init.lua
@@ -0,0 +1,32 @@
+local love = require('love')
+local world = love.physics.newWorld(0, 0)
+local sti = require("lib/sti")
+local maps = require("maps")
+
+return {
+  map00 = {
+    map = sti(maps.world01.m00, { "box2d" }),
+    scale = 1,
+
+    load = function(self)
+      self.scale = love.graphics.getHeight() / (self.map.height * self.map.tileheight)
+      love.audio.stop()
+      self.map:box2d_init(world)
+      self.map.layers.solid.visible = false
+    end,
+
+    update = function(self, dt)
+      self.scale = love.graphics.getHeight() / (self.map.height * self.map.tileheight)
+      world:update(dt)
+    end,
+
+    draw = function(self)
+      love.graphics.setColor(1, 1, 1)
+      self.map:draw(0, 0, self.scale, self.scale)
+
+      love.graphics.push()
+      love.graphics.scale(self.scale, self.scale)
+      love.graphics.pop()
+    end,
+  }
+}
--- screens/game.lua
+++ screens/game.lua
@@ -1,23 +1,17 @@
-local love = require("love")
-local sti = require("lib/sti")
-local maps = require("maps")
+local world01 = require("maps.world01")
 
 local function gameScreen()
-  local map = sti(maps.world01.m00)
-  local scaleFactor = 1
-
   return {
     load = function(self)
-      love.audio.stop()
+      world01.map00:load()
     end,
 
     update = function(self, dt)
-      scaleFactor = love.graphics.getWidth() / 1920 * 10
+      world01.map00:update(dt)
     end,
 
     draw = function(self)
-      love.graphics.setColor(1, 1, 1)
-      map:draw(0, 0, scaleFactor, scaleFactor)
+      world01.map00:draw()
     end,
   }
 end
