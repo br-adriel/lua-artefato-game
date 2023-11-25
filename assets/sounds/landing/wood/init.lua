44470651afab49f4fcbb5060695c1013526019cb
Adriel <adriel.fsantos@outlook.com>
2023-11-24 23:50:13 -0300

    Adiciona efeitos sonoros de aterrissagem

---
+++ assets/sounds/landing/wood/init.lua
@@ -0,0 +1,14 @@
+return {
+  small = {
+    source = "assets/sounds/landing/wood/small.mp3",
+    type = "static"
+  },
+  medium = {
+    source = "assets/sounds/landing/wood/medium.mp3",
+    type = "static"
+  },
+  large = {
+    source = "assets/sounds/landing/wood/large.mp3",
+    type = "static"
+  },
+}
--- maps/world01/init.lua
+++ maps/world01/init.lua
@@ -2,12 +2,22 @@ local love = require('love')
 local sti = require("lib/sti")
 local maps = require("maps")
 local world = love.physics.newWorld(0, 0)
+local woodSounds = require("assets.sounds.landing.wood")
 
 return {
   world = world,
   map00 = {
     map = sti(maps.world01.m00, { "box2d" }),
     scale = 1,
+    sounds = {
+      player = {
+        landing = {
+          small = love.audio.newSource(woodSounds.small.source, woodSounds.small.type),
+          medium = love.audio.newSource(woodSounds.medium.source, woodSounds.medium.type),
+          large = love.audio.newSource(woodSounds.large.source, woodSounds.large.type),
+        }
+      }
+    },
 
     load = function(self)
       _G.world = world
