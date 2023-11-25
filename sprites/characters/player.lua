0732e7c5d64f30746cb0034a8fc2b325970aefa2
Adriel <adriel.fsantos@outlook.com>
2023-11-25 19:06:44 -0300

    Adiciona sprite de pulo

--- sprites/characters/player.lua
+++ sprites/characters/player.lua
@@ -1,18 +1,28 @@
 local anim8 = require('lib.anim8')
 
-local function Player(frameDuration)
-  local d = frameDuration or 0.1
-  local sprite = _G.love.graphics.newImage('assets/sprites/characters/player.png')
-  local grid = anim8.newGrid(20, 32, sprite:getWidth(), sprite:getHeight())
+local function Player()
+  local durationIdle = 0.2
+  local spriteIdle = _G.love.graphics.newImage('assets/sprites/characters/player/idle.png')
+  local gridIdle = anim8.newGrid(20, 32, spriteIdle:getWidth(), spriteIdle:getHeight())
+
+  local spriteJump = _G.love.graphics.newImage('assets/sprites/characters/player/jump.png')
+  local gridJump = anim8.newGrid(25, 37, spriteJump:getWidth(), spriteJump:getHeight())
 
   return {
     animations = {
       idle = {
-        left = anim8.newAnimation(grid('1-10', 1), d),
-        right = anim8.newAnimation(grid('1-10', 2), d),
+        left = anim8.newAnimation(gridIdle('1-10', 1), durationIdle),
+        right = anim8.newAnimation(gridIdle('1-10', 2), durationIdle),
+      },
+      jump = {
+        left = anim8.newAnimation(gridJump(1, 2), 1),
+        right = anim8.newAnimation(gridJump(1, 1), 1),
       }
     },
-    sprite = sprite,
+    sprite = {
+      idle = spriteIdle,
+      jump = spriteJump,
+    },
   }
 end
 
