b3679dbf6d4ce0994b1ec5ea9a8d0c064b9d7be2
Adriel <adriel.fsantos@outlook.com>
2023-11-25 22:49:51 -0300

    Implementa animaçao e sons de deslocamento

--- sprites/characters/player.lua
+++ sprites/characters/player.lua
@@ -8,6 +8,10 @@ local function Player()
   local spriteJump = _G.love.graphics.newImage('assets/sprites/characters/player/jump.png')
   local gridJump = anim8.newGrid(25, 37, spriteJump:getWidth(), spriteJump:getHeight())
 
+  local durationWalk = 0.15
+  local spriteWalk = _G.love.graphics.newImage('assets/sprites/characters/player/walk.png')
+  local gridWalk = anim8.newGrid(23, 32, spriteWalk:getWidth(), spriteWalk:getHeight())
+
   return {
     animations = {
       idle = {
@@ -17,12 +21,22 @@ local function Player()
       jump = {
         left = anim8.newAnimation(gridJump(1, 2), 1),
         right = anim8.newAnimation(gridJump(1, 1), 1),
-      }
+      },
+      walk = {
+        left = anim8.newAnimation(gridWalk('1-8', 2), durationWalk),
+        right = anim8.newAnimation(gridWalk('1-8', 1), durationWalk),
+      },
     },
     sprite = {
       idle = spriteIdle,
       jump = spriteJump,
+      walk = spriteWalk,
     },
+    grids = {
+      idle = gridIdle,
+      jump = gridJump,
+      walk = gridWalk
+    }
   }
 end
 
